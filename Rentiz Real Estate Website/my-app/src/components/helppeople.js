80ecc837c8320b00cf46298339bb33649be9521b
unknown <danishjajja86@gmail.com>
2025-03-21 00:25:00 +0500

    2nd deployment of Rentiz app

--- Rentiz Real Estate Website/my-app/src/components/helppeople.js
+++ Rentiz Real Estate Website/my-app/src/components/helppeople.js
@@ -12,7 +12,7 @@ export default function HelpPeople() {
         </div>
         <div className="text-container">
           <h1>We help people to find homes</h1>
-          <h6>Mauris orci donec blandit maecenas. Orci lorem purus porttitor massa consectetur. Neque, vestibulum sed varius magna et at. Eu, adipiscing morbi augue justo. Nibh laoreet volutpat quis velit. Blandit aliquam donec sed morbi congue eget lorem viverra porta id lobortis.</h6>
+          <h6>Mauris orci donec blandit macenas. Orci lorem purus porttitor massa consectetur. Neque, vestibulum sed varius magna et at. Eu, adipiscing morbi augue justo. Nibh laoreet volutpat quis velit. Blandit aliquam donec sed morbi congue eget lorem viverra porta id lobortis.</h6>
           <img src="/getintouch.png" alt="Get In Touch" />
         </div>
 
